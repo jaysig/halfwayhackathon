563c18f7da4b4390f031af0fabba603eda144985
Jaysig <jaytomsig@gmail.com>
2015-09-04 13:31:02 -0400

    errors with toggle

---
+++ js/choices.js
@@ -0,0 +1,7 @@
+var jq = function() {
+  $('#name-quest').slideToggle();
+  return false;
+};
+var name =
+  $('#choice1').append("Welcome " + name + ". I do not think we have met before. Welcome to Mr. Bigglesworth fortress.
+  I am really bad with names; so, I will call you seeker. I hope you do not mind. Now seeker why are you here?");
